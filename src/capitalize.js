a9e2b857291a7e90c6c1d0010e67c88f1741e3b5
MariiaUlankina <ulankina.masha@gmail.com>
2021-12-20 17:14:18 +0200

    added function capitalize

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,12 @@
 function capitalize(string) {
+    let arr = string.split(' ');
+    let newArr = arr.map(function(item){
+        let word =item.slice(1);
+        let result = item[0].toUpperCase();
+        return `${result}${word}`
+    })
+    return newArr.join(' ')
 }
+
 
 module.exports = capitalize;
